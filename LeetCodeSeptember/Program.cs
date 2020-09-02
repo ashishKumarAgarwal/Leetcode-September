01cd50531de7b1c9596d0ae366580578f2cf842e
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-09-02 22:52:03 +0530

    sep1 channel

---
+++ LeetCodeSeptember/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCodeSeptember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sep1 sep1=new Sep1();
+            Console.WriteLine(sep1.LargestTimeFromDigits(new[] { 1, 2, 3, 4 }));
+         
+            
+        }
+    }
+}
---
+++ LeetCodeSeptember/Sep1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSeptember
+{
+    class Sep1
+    {
+        //Input: [1,2,3,4]
+        //Output: "23:41"
+        //Input: [5,5,5,5]
+        //Output: ""
+        public string LargestTimeFromDigits(int[] A)
+        {
+            var result = "";
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    for (int k = 0; k < 4; ++k)
+                    {
+                        if (i == j || j == k || k == i) continue;
+                        var hh = A[i] + "" + A[j];
+                        var mm = A[k] + "" + A[6 - i - j - k];
+                        var _time = hh + ":" + mm;
+
+                        if (string.Compare(hh, "24", StringComparison.Ordinal) < 0
+                            && string.Compare(mm, "60", StringComparison.Ordinal) < 0
+                            && string.Compare(_time, result, StringComparison.Ordinal) > 0)
+                        {
+                            result = _time;
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
